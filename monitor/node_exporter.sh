d9b146d55d71cf7424e27f7b62a545d20249f6c0
kosta709 <kosta@codefresh.io>
2018-04-23 19:50:45 +0300

    added ENABLED_COLLECTORS env for node_exporter

--- monitor/node_exporter.sh
+++ monitor/node_exporter.sh
@@ -2,6 +2,11 @@
 #
 
 DIR=$(dirname $0)
+TEXTFILE_DIRECTORY=${DIR}/metrics
+mkdir -p ${TEXTFILE_DIRECTORY}
+echo "Starting node_exporter at $(date):
+   TEXTFILE_DIRECTORY = ${TEXTFILE_DIRECTORY}
+"
 
 DISABLED_COLLECTORS=(arp bcache bonding buddyinfo conntrack cpu diskstats drbd edac entropy filefd filesystem gmond hwmon infiniband \
         interrupts ipvs ksmd loadavg logind mdadm megacli meminfo meminfo_numa mountstats netdev netstat nfs ntp qdisc \
@@ -9,13 +14,11 @@ DISABLED_COLLECTORS=(arp bcache bonding buddyinfo conntrack cpu diskstats drbd e
 
 DISABLE_COLLECTORS_ARGS=""
 for i in ${DISABLED_COLLECTORS[@]}; do
+   if [[ -n "${ENABLED_COLLECTORS}" && "${i}" =~ ${ENABLED_COLLECTORS} ]]; then
+      echo "node_exporter - Enabling collector $i "
+      continue
+   fi
    DISABLE_COLLECTORS_ARGS="${DISABLE_COLLECTORS_ARGS} --no-collector.${i}"
 done
-
-TEXTFILE_DIRECTORY=${DIR}/metrics
-mkdir -p ${TEXTFILE_DIRECTORY}
-echo "Starting node_exporter at $(date):
-   TEXTFILE_DIRECTORY = ${TEXTFILE_DIRECTORY}
-"
 
 node_exporter ${DISABLE_COLLECTORS_ARGS} --collector.textfile --collector.textfile.directory=${TEXTFILE_DIRECTORY}
