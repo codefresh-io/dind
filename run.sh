12111b9e179f3872ea4dfd213a5042cf564931ad
Zhenya Tikhonov <zhenya.tikhonov@codefresh.io>
2025-09-16 19:23:51 +0400

    fix: move containerd process to init cgroup

--- run.sh
+++ run.sh
@@ -194,6 +194,15 @@ do
     echo "Using cgroup v2"
     CURRENT_CGROUP=$(cat /proc/self/cgroup | sed 's/0:://')
     echo "Current cgroup: ${CURRENT_CGROUP}"
+    # move the processes from the root group to the /init group,
+    # otherwise writing subtree_control fails with EBUSY.
+    # An error during moving non-existent process (i.e., "cat") is ignored.
+    mkdir -p ${CURRENT_CGROUP}/init
+    xargs -rn1 < ${CURRENT_CGROUP}/cgroup.procs > ${CURRENT_CGROUP}/init/cgroup.procs || :
+    # # enable controllers
+    # sed -e 's/ / +/g' -e 's/^/+/' < ${CURRENT_CGROUP}/cgroup.controllers \
+    #   > ${CURRENT_CGROUP}/cgroup.subtree_control
+
     MEMORY_OOM_GROUP="/sys/fs/cgroup/${CURRENT_CGROUP}/memory.oom.group"
     echo "Ensuring memory.oom.group is set to 0 to disable killing all processes in cgroup on OOM"
     echo "0" > "${MEMORY_OOM_GROUP}"
